56b4d56c24ef9cf9c9d3d35212afc1f6db026d4e
Vericel Gregory <gregoryvericel6@gmail.com>
2024-06-22 11:04:19 +0200

    update init function in index.js file

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -28,7 +28,7 @@ async function displayData(photographers) {
 
 async function init() {
   // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
+  const photographers = await getPhotographers();
   displayData(photographers);
 }
 
